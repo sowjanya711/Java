c0d454a2121840ec0f4c52132e511033acee5976
Sowjanya <sowji.sowjee@gmail.com>
2017-08-20 22:49:03 -0400

    Added String Buffer Methods

---
+++ Java/src/com/stringBuilder/java/StringBuilderMethods.java
@@ -0,0 +1,20 @@
+package com.stringBuilder.java;
+
+/*
+ * String Builder is same as String Buffer except that it is not synchronized
+ */
+/*
+ * StringBuffer is synchronized i.e. thread safe. 
+ * It means two threads can't call the methods of StringBuffer simultaneously.
+ * LESS Efficient
+ */
+
+/*
+ * StringBuilder is non-synchronized i.e. not thread safe.
+ * It means two threads can call the methods of StringBuilder simultaneously.
+ * 
+ * MORE Efficient
+ */
+public class StringBuilderMethods {
+
+}
