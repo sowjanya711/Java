8e4ef72696e5ecff6633ebafccbb1d479663e2ae
Sowjanya <sowji.sowjee@gmail.com>
2017-09-06 08:11:07 -0400

    Added Final Keyword Definition

--- Java/src/com/definitions/java/Definitions.java
+++ Java/src/com/definitions/java/Definitions.java
@@ -8,7 +8,12 @@ Object : Object is an instance of class. Object has state and behavior
 		 Calling methods using object : s.totalMarks(), s.Rank()
 
 Static Variables:  Static variables are at class level. 
-				   When a variable is declared as static, all the objects of its class share the same static variable				
+				   When a variable is declared as static, all the objects of its class share the same static variable
+				   
+Final Keyword: Final variable cannot be changed (once assigned a value, it cannot be changed)
+			   Final method cannot be overriden (method implementation code cannot be changed)
+			   Final Class cannot be extended
+			   
 Inheritance :  Acquiring the properties of a parent class is called Inheritance
 		Class can be inherited using "extends" keyword
 		Method OverRiding can be achieved by inheritance
@@ -17,11 +22,11 @@ Polymorphism : The ability of an object to take many forms is known as Polymorph
 		1. Method Overloading 2. Method OverRiding
 					
 Method Overloading : Two or more methods having same name but different parameters ( by changing no of arguments, 
-					 by changing data type)
+					 by changing data type)  e.g.: add Method
 					 
 Method OverRiding :  Let's say there are 2 classes. When a method in child class has the same method name and 
 					 type signature as method in its super class, then the method in the sub class 
-					 is said to be override the method in the super class
+					 is said to be override the method in the super class e.g.: rateOfInterset Method
 
 Abstraction : Abstraction is a process of hiding the implementation details and showing only functionality to the user.
 			  Ways to achieve:
@@ -54,7 +59,7 @@ Interface :
 	3.	Interface can't be instantiated -- can't create an object for instance 
 	4.	Interface is used to achieve data abstraction and multiple inheritance	
 					 
-Encapsulation : Wrapping up of data into a single unit. Access Modifiers
+Encapsulation : Wrapping up of data into a single unit. It provides control over the data. Access Modifiers
 
 Constructor: It is a special type of method , it is invoked at the time of creation of object.
  			Rules:
@@ -62,7 +67,9 @@ Constructor: It is a special type of method , it is invoked at the time of creat
   	2. Constructor must not have explicit return type
   	
 Array : It is a collection of similar type of elements that have a memory location
-		We can have only fixed size of array, it cannot grow its size in runtime. 
+		We can have only fixed size of array, it cannot grow its size in runtime.
+		
+		 syntax : int a [] = new int[5];
 
  dataType arr[];  --> declaration
  new dataType[size] --> Instantiation
