83ecccc7873986d540a6c4e5b239a46c9e698449
Sowjanya <sowji.sowjee@gmail.com>
2017-12-23 19:28:04 -0500

    Modified Definitions

--- Java/src/com/definitions/java/Definitions.java
+++ Java/src/com/definitions/java/Definitions.java
@@ -1,47 +1,49 @@
 /* Class : 	Class is a template using which we can create an object
-
+   -----
+   	
 Object : Object is an instance of class. Object has state and behavior
-	 	 Object name should be class name for which we are we want to create an object 
+-------	 Object name should be class name for which we are we want to create an object 
 		 class : Student
 		 class Student Methods : totalMarks() , Rank()
 		 Object Creation : Student s = new Student();
 		 Calling methods using object : s.totalMarks(), s.Rank()
 
 Static Variables:  Static variables are at class level. 
-				   When a variable is declared as static, all the objects of its class share the same static variable
+----------------   When a variable is declared as static, all the objects of its class share the same static variable
 				   
 Final Keyword: Final variable cannot be changed (once assigned a value, it cannot be changed)
-			   Final method cannot be overriden (method implementation code cannot be changed)
+-------------  Final method cannot be overridden (method implementation code cannot be changed)
 			   Final Class cannot be extended
 			   
 Inheritance :  Acquiring the properties of a parent class is called Inheritance
-		Class can be inherited using "extends" keyword
-		Method OverRiding can be achieved by inheritance
+------------   Class can be inherited using "extends" keyword
+			   Method OverRiding can be achieved by inheritance
 
 Polymorphism : The ability of an object to take many forms is known as Polymorphism.
-		1. Method Overloading 2. Method OverRiding
+------------   1. Method Overloading 2. Method OverRiding
 					
 Method Overloading : Two or more methods having same name but different parameters ( by changing no of arguments, 
-					 by changing data type)  e.g.: add Method
+------------------	 by changing data type)  e.g.: add Method
 					 
 Method OverRiding :  Let's say there are 2 classes. When a method in child class has the same method name and 
-					 type signature as method in its super class, then the method in the sub class 
+-----------------	 type signature as method in its super class, then the method in the sub class 
 					 is said to be override the method in the super class e.g.: rateOfInterset Method
 
 Abstraction : Abstraction is a process of hiding the implementation details and showing only functionality to the user.
-			  Ways to achieve:
+-----------   Ways to achieve:
 			  1. Abstract Class
 			  2. Interface
 			 
 Abstract Class :  A class that is declared as Abstract is an abstract class
+--------------
 
 Abstract Method:  A method that is declared as abstract and does not have implementation is known as abstract method.
-
+---------------
 	1.	Class needs to be extended and its method needs to be implemented.
 	2.	 It cannot be instantiated -- cannot create object for the abstract class.
  
 Interface :
- 
+--------- 
 	Notes:
 	•	Interface is like Menu (blue print of class), Class is like Menu with Recipe Details
 	•	In interface you declare methods but implementation of the method should be done in the class that implements 
@@ -53,24 +55,26 @@ Interface :
 	•	The variables must also be initialized with a constant value. i.e value should be assigned.
 	
  Definiton:
-	 
+----------	 
 	1.	Interface is a blueprint of class, there can be only abstract methods in instance
 	2.	It is used to achieve abstraction and multiple inheritance in Java. 
 	3.	Interface can't be instantiated -- can't create an object for instance 
 	4.	Interface is used to achieve data abstraction and multiple inheritance	
 					 
 Encapsulation : Wrapping up of data into a single unit. It provides control over the data. Access Modifiers
+-------------
 
 Constructor: It is a special type of method , it is invoked at the time of creation of object.
- 			Rules:
+-----------
+			Rules:
   	1. Constructor name must be the same name as its class name
   	2. Constructor must not have explicit return type
   	
 Array : It is a collection of similar type of elements that have a memory location
-		We can have only fixed size of array, it cannot grow its size in runtime.
+-----	We can have only fixed size of array, it cannot grow its size in runtime.
 		
 		 syntax : int a [] = new int[5];
-
+		 ------
  dataType arr[];  --> declaration
  new dataType[size] --> Instantiation
 
